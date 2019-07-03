0a4af0369128c9298672417b0154b2e12d05e78f
Matt Gatner <mgatner@tattersoftware.com>
2019-07-03 16:03:05 +0000

    Add language support

---
+++ src/Language/en/Permits.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+	'missingDatabaseTable'  => 'Table "{0}" missing for permissions handling."',
+	'invalidModelType'      => 'Invalid permit mode type on model for "{0}"',
+	'invalidMode'           => 'Invalid permit mode on model for "{0}": {1}',
+];
